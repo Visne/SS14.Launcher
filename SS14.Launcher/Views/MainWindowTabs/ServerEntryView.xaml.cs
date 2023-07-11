1ab670ef8ea3b01b12ffab480192e6cc9362c5a9
Visne <39844191+Visne@users.noreply.github.com>
2023-07-12 01:42:29 +0200

    RowSideAlignedPanel, make link buttons fancy

--- SS14.Launcher/Views/MainWindowTabs/ServerEntryView.xaml.cs
+++ SS14.Launcher/Views/MainWindowTabs/ServerEntryView.xaml.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Linq;
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Controls.Presenters;
+using Avalonia.LogicalTree;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
 
 namespace SS14.Launcher.Views.MainWindowTabs;
@@ -10,6 +13,32 @@ public partial class ServerEntryView : UserControl
     public ServerEntryView()
     {
         InitializeComponent();
+
+        Links.LayoutUpdated += ApplyStyle;
+    }
+
+    // Sets the style for the link buttons correctly so that they look fancy
+    private void ApplyStyle(object? _1, EventArgs _2)
+    {
+        for (var i = 0; i < Links.ItemCount; i++)
+        {
+            var presenter = Links.ItemContainerGenerator.ContainerFromIndex(i);
+            presenter.ApplyTemplate();
+
+            if (presenter is not ContentPresenter { Child: ServerInfoLinkControl control }) continue;
+
+            string? style;
+            if (Links.ItemCount == 1)
+                return;
+            else if (i == 0)
+                style = "OpenRight";
+            else if (i == Links.ItemCount - 1)
+                style = "OpenLeft";
+            else
+                style = "OpenBoth";
+
+            control.GetLogicalChildren().OfType<Button>().FirstOrDefault()?.Classes.Add(style);
+        }
     }
 
     protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
